9a52d93a84619c825b9ceee859b34c86287bbf39
Sidnei Repetcki <sid.r.junior@hotmail.com>
2026-04-26 00:36:15 -0300

    feat: estilização e diferenciação dos filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,14 +4,20 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
 
-    const options = [
-        { value: 'Rodrigo', label: 'Rodrigo' },
-        { value: 'Maria', label: 'Maria' },
-        { value: 'Ana', label: 'Ana' },
+    const months = [
+        { value: 4, label: 'Abril' },
+        { value: 5, label: 'Maio' },
+        { value: 6, label: 'Junho' },
+    ];
+
+    const years = [
+        { value: 2023, label: 2023 },
+        { value: 2024, label: 2024 },
+        { value: 2025, label: 2025 },
     ];
 
     return (
@@ -19,14 +25,31 @@ const List: React.FC = () => {
             <Container>
 
                 <ContentHeader title="Saídas" lineColor="#E44C4E">
-                    <SelectInput options={options} />
+                    <SelectInput options={months} />
+                    <SelectInput options={years} />
                 </ContentHeader>
+
+                <Filters>
+                    <button 
+                            type="button"
+                            className="tag-filter tag-filter-recurrent"
+                        >
+                        Recorrentes
+                    </button>
+                
+                    <button 
+                            type="button"
+                            className="tag-filter tag-filter-eventual"
+                        >
+                        Eventuais
+                    </button>
+                </Filters>
 
                 <Content>
                     <HistoryFinanceCard
                         tagColor="#E44C4E"
                         title="Conta de Luz"
-                        subtitle="23/04/2026"
+                        subtitle="23/04/2023"
                         amount="R$ 130,00"
                     />
 
